8acb7d2b9e2e88861637285506c5c1256187710a
Igor Clemente <igorelectrofest2011@hotmail.com>
2020-01-24 22:45:47 -0300

    BOOTCAMP_GOSTACK_MODULE15_CALCULEDISTANCEWITHCOORDINATES_CREATED_24012020

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,6 +3,8 @@ const Dev = require("../models/Dev");
 
 const parseStringAsArray = require("../utils/parseStringAsArray");
 
+const { filterConnections, sendMessage } = require("../websocket");
+
 module.exports = {
   async index(request, response) {
     const devs = await Dev.find();
@@ -39,6 +41,13 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      const sendSocketMessageTo = filterConnections(
+        { latitude, longitude },
+        techsArray
+      );
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     response.json(dev);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 
-const server = express();
+const app = express();
+const server = http.Server(app);
 
 const routes = require("./routes");
+
+const { setupWebSocket } = require("./websocket");
+
+setupWebSocket(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-az520.mongodb.net/week10?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
